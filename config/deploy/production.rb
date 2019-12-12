b6dd0a01baba75efc15c73ffa553d7b4b3406485
falex1022 <56875497+falex1022@users.noreply.github.com>
2019-12-12 11:43:11 +0900

    フォーク後自動デプロイ構築中

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.177.59.127', user: 'ec2-user', roles: %w{app db web}
+server '13.114.55.210', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
